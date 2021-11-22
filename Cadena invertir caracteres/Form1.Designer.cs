64670a99539ad33aa0627f8a4fa3c60971904481
Leandro Santana <santanaleandro099@gmail.com>
2021-11-21 22:50:46 -0600

    Cadena invertir caracteres

---
+++ Cadena invertir caracteres/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Cadena_invertir_caracteres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCadena = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.btEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese palabra";
+            // 
+            // txtCadena
+            // 
+            this.txtCadena.Location = new System.Drawing.Point(203, 64);
+            this.txtCadena.Name = "txtCadena";
+            this.txtCadena.Size = new System.Drawing.Size(292, 20);
+            this.txtCadena.TabIndex = 1;
+            this.txtCadena.TextChanged += new System.EventHandler(this.txtCadena_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultado";
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(203, 184);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(292, 20);
+            this.txtRes.TabIndex = 3;
+            // 
+            // btEnviar
+            // 
+            this.btEnviar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEnviar.Location = new System.Drawing.Point(573, 71);
+            this.btEnviar.Name = "btEnviar";
+            this.btEnviar.Size = new System.Drawing.Size(153, 52);
+            this.btEnviar.TabIndex = 4;
+            this.btEnviar.Text = "Enviar";
+            this.btEnviar.UseVisualStyleBackColor = true;
+            this.btEnviar.Click += new System.EventHandler(this.btEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btEnviar);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCadena);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCadena;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.Button btEnviar;
+    }
+}
+
---
+++ Cadena invertir caracteres/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cadena_invertir_caracteres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCadena_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btEnviar_Click(object sender, EventArgs e)
+        {
+            string mostar = this.txtCadena.Text;
+            string conta = "";
+            for (int x = mostar.Length-1; x>= 0 ;x--)
+            {
+                conta = conta + mostar[x];
+                
+            }
+            this.txtRes.Text = conta.ToString();
+        }
+    }
+}
